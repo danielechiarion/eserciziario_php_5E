dbda1a7437eab7a354e5aa7b91aac2a262cb9144
docente1 <docente1@internal.iisviolamarchesini.edu.it>
2026-03-03 11:44:52 +0100

    add code for table

---
+++ Chiarion_Es2A_table-module/index.php
@@ -0,0 +1,12 @@
+<?php
+include('table.inc');
+?>
+
+<!DOCTYPE html>
+<head>
+    <title>Table module</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <?php drawTable(7,10); ?>
+</body>
